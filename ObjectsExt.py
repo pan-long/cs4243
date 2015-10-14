8d922b88e5fbd6475500e3e6428f920c22023866
Pan Long <aga.panlong@gmail.com>
2015-10-14 09:37:49 +0800

    Extract objects using BRG image instead of grayscale image.

--- ObjectsExt.py
+++ ObjectsExt.py
@@ -7,24 +7,23 @@ class ObjectsExt(object):
     Extract objects from a image. A background image is required to be compared with the image passed in.
     """
     def __init__(self, background):
-        self.background = cv2.cvtColor(background, cv2.COLOR_BGR2GRAY)
+        self.background = background
 
     def extract_objects(self, image, threshold=20):
         """
         Extract objects in image.
         :param image: The image that the objects are from.
-        :param threshold: The threshold of the difference in grayscale to extract objects.
+        :param threshold: The threshold of the difference in any of BGR to extract objects.
         :return: An image that contains only the objects.
         """
-        image_grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = np.array(image, dtype=int)
         width, height = image.shape[:2]
-        result_img = np.zeros((width, height))
+        result_img = np.zeros((width, height, 3))
         for i in range(height):
             for j in range(width):
-                # * 1 to convert from unit8 to int to avoid overflow.
-                if abs(image_grayscale[j, i] * 1 - self.background[j, i]) < threshold:
-                    result_img[j, i] = 255
+                if any(d > threshold for d in abs(image[j, i] - self.background[j, i])):
+                    result_img[j, i] = image[j, i]
                 else:
-                    result_img[j, i] = image_grayscale[j, i]
+                    result_img[j, i] = [255, 255, 255]
 
         return cv2.convertScaleAbs(result_img)
