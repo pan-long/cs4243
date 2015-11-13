abc17c3aab59b38dca4ab4b47c42ac166a306449
richada <richardlab522@gmail.com>
2015-11-14 01:12:26 +0800

    Redefine calDist

--- distCal.py
+++ distCal.py
@@ -17,12 +17,10 @@ class distCal (object):
         self.distance = 0
         self.actual_distance = 0
 
-    def getDist(self,x,y):
-        distance = 0
-        for i in range (len(x)-1):
-            distance += np.sqrt((x[i+1]-x[i]) ** 2 + (y[i+1]-y[i]) ** 2)
+    def getDist(self,prevD,x1,x2,y1,y2):
+        distance = np.sqrt((x2-x1) ** 2 + (y2-y1) ** 2)
 
         actual_distance = distance / 1067 * 110
 
-        return actual_distance
+        return actual_distance + prevD
 
